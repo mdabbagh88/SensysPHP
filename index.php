8b146bc2a8327fca3b436878e0bb124144625a41
Endru Reza <lotusb13@gmail.com>
2014-01-04 05:31:44 +0700

    Update some info on the page

--- index.php
+++ index.php
@@ -7,8 +7,98 @@
     	<h1>Home<small> Latest Update!</small></h1>
     </div>
 </div>
-
-
+<div class="alert alert-success alert-dismissable">
+	<button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+    You have 1.1 Beta Version ! You can browse SensysPHP version <a class="alert-link" href="http://github.com/endrureza/sensysphp/"><i class="fa fa-github-alt"></i>Github</a>{Feel Free to use this on your project}
+</div>
+<div class="row">
+	<div class="col-lg-3">
+    	<div class="panel panel-success">
+        	<div class="panel-heading">
+            	<div class="row">
+                	<div class="col-xs-6">
+                    	<i class="fa fa-thumbs-up fa-5x"></i>
+                    </div>
+                    <div class="col-xs-6 text-right">
+                        <p class="announcement-text">Based On</p>
+                        <p class="announcement-heading">PHP</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3">
+    	<div class="panel panel-info">
+        	<div class="panel-heading">
+            	<div class="row">
+                	<div class="col-xs-6">
+                    	<i class="fa fa-thumbs-up fa-5x"></i>
+                    </div>
+                    <div class="col-xs-6 text-right">
+                        <p class="announcement-text">Bootstrape</p>
+                        <p class="announcement-heading">Cool</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3">
+    	<div class="panel panel-warning">
+        	<div class="panel-heading">
+            	<div class="row">
+                	<div class="col-xs-6">
+                    	<i class="fa fa-dollar fa-5x"></i>
+                    </div>
+                    <div class="col-xs-6 text-right">
+                        <p class="announcement-text">Need to Pay ?</p>
+                        <p class="announcement-heading">Free</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="col-lg-3">
+    	<div class="panel panel-danger">
+        	<div class="panel-heading">
+            	<div class="row">
+                	<div class="col-xs-6">
+                    	<i class="fa fa-wheelchair fa-5x"></i>
+                    </div>
+                    <div class="col-xs-6 text-right">
+                        <p class="announcement-text">Support</p>
+                        <p class="announcement-heading">Look</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="row">
+	<div class="col-lg-4">
+    	<h1>New Features</h1>
+        <h4>Crawling Update !</h4>
+        <blockquote>
+        	<p>Integrate with graph api so that any website has fb social plugin will be processed</p>
+        </blockquote>
+    </div>
+    <div class="col-lg-4">
+        <h1>Our Social Network</h1>
+    	<div class="col-xs-10 text-center">
+        	<a href="http://www.facebook.com/sysfotech"><i class="fa fa-facebook-square fa-5x"></i></a>
+            <a href="http://www.twitter.com/sysfotech"><i class="fa fa-twitter-square fa-5x"></i></a>
+            <a href="http://www.github.com/endrureza"><i class="fa fa-github-square fa-5x"></i></a>
+        </div>
+    </div>
+    <div class="col-lg-4">
+    	<h1>Opt. Browser</h1>
+        <ul>
+        	<li>Google Chrome</li>
+            <li>Firefox</li>
+            <li>Opera</li>
+            <li>Safari</li>
+        </ul>
+    </div>
+</div>
 <?php
     include ("footer.php");
 ?>
