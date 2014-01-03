343384c349f4fb486f565a9c2e9ca3a8d87948e0
Endru Reza <lotusb13@gmail.com>
2014-01-04 05:30:52 +0700

    Under Construct

--- about.php
+++ about.php
@@ -4,10 +4,16 @@
 
 <div class="row">
 	<div class="col-lg-12">
-    	<h1>About Tools<small> and The Developer</small></h1>
+    	<h1>About Tools<small> The Developer and his Documentation</small></h1>
     </div>
 </div>
-
+<div class="row">
+	<div class="col-lg-12">
+    	<div class="col-xs-24 text-center">
+        	<h1>This Function Page is Under Construct !</h1>
+        </div>
+    </div>
+</div>
 
 <?php
     include ("footer.php");
