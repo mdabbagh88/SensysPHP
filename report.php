ba7dc507d19132c4d1aa5e2b9c199f7e92d18f78
Endru Reza <lotusb13@gmail.com>
2014-01-04 05:32:23 +0700

    Under Construct

--- report.php
+++ report.php
@@ -8,6 +8,14 @@
     </div>
 </div>
 
+<div class="row">
+	<div class="col-lg-12">
+    	<div class="col-xs-24 text-center">
+        	<h1>This Function Page is Under Construct !</h1>
+        </div>
+    </div>
+</div>
+
 
 <?php
     include ("footer.php");
