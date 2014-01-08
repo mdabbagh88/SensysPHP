b8d84c54f00b48d6e0a71b72c21f90d74743ddb3
Endru Reza <lotusb13@gmail.com>
2014-01-08 23:17:16 +0700

    Update some interface, add some settings

--- crawl.php
+++ crawl.php
@@ -15,12 +15,12 @@ class MyCrawler extends PHPCrawler
         if (PHP_SAPI == "cli") $lb = "\n";
         else $lb = "<br>";
         echo "<tr>";
-        echo "<td>".$DocInfo -> url."</td>";
+        echo "<td><a href=".$DocInfo -> url.">".$DocInfo -> url."</a></td>";
         echo "<td>".$DocInfo -> referer_url."</td>";
         echo "<td>".$DocInfo -> http_status_code."</td>";
         echo "</tr>";
         
-        $file ="Data.txt";
+        $file ="results/".$_REQUEST['file'].".txt";
         
         $konten = fopen($file,'a');
         
@@ -54,64 +54,98 @@ class MyCrawler extends PHPCrawler
     	You may enter a website below
     </div>
 </div>
-
+<form role="form" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
 <div class="row">
-	<div class="col-lg-6">
-    	<form role="form" method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
-            <div class="form-group">
+	<div class="col-lg-12">
+        <h2>Crawler Setting</h2>
+        <div class="col-xs-6">
+    	    <div class="form-group">
+                <fieldset>
+                <legend>Basic Setting</legend>
         		<label>Website URL :</label>
             	<input class="form-control" placeholder="Ex: www.sysfotech.com" name="url" required="true"><br>
                 <label>Follows Rule :</label>
                 <input class="form-control" placeholder="Ex: #www.sysfotech.com/post#" name="rule"><br>
-                <button type="submit" name="submit" class="btn btn-default">
-                Start Crawling
-                </button>
+                <label>File Name:</label>
+                <input class="form-control" placeholder="Ex: Data" name="file" required="true"><br>
+                </fieldset>
             </div>
-        </form>
+        </div>
+        <div class="col-xs-6">
+            <div class="form-group">
+                <fieldset>
+                <legend>Advanced Setting</legend>
+                <label>Filter Rule:</label>
+                <select multiple class="form-control">
+                	<option value="jpeg">JPEG</option>
+                    <option value="gif">GIF</option>
+                    <option value="css">CSS</option>
+                    <option value="jpg">JPG</option>
+                    <option value="png">PNG</option>
+                    <option value="js">JS</option>
+                    <option value="bmp">BMP</option>
+                </select><br>
+                <label>Content Type Received:</label>
+                <select class="form-control">
+                    <option value="#text/html#">default</option>
+                	<option value="#text/html#">text/html</option>    
+                </select>
+                </fieldset>
+            </div>
+        </div>
     </div>
 </div>
-
+<div class="row">
+    <div class="col-lg-12 text-center">
+         <div class="form-group">
+               <button type="submit" name="submit" class="btn btn-primary">
+               Start Crawling
+               </button>
+         </div>
+    </div>
+</div>
+</form>
 <div class="row">
 	<div class="col-lg-12">
-    	<h2>Your data Will be written on the table below</h2>
+    	<h2>Crawling Result</h2>
         <div class="table-responsive">
-        	<table class="table table-boardered table-hover table-striped tablesorter">
+        	<table class="table table-bordered table-hover table-striped tablesorter">
             	<thead>
                 	<tr>
-                    	<th>Page Requested</th>
-                        <th>Page Referer</th>
-                        <th>Http Status Code</th>
+                    	<th>Page Requested<i class="fa fa-sort"></i></th>
+                        <th>Page Referer<i class="fa fa-sort"></i></th>
+                        <th>Http Status Code<i class="fa fa-sort"></i></th>
                     </tr>
-                    <?php
-			if(!isset($_POST['submit']))
-			{
-                    
-                    	}else
-                        {
-	                        $crawler = new MyCrawler();
-	                            
-	                        $crawler -> setURL($_POST['url']);
-	                        
-	                        $crawler -> addURLFollowRule($_POST['rule']);
-	                        
-	                        $crawler -> addContentTypeReceiveRule("#text/html#");
-	                        
-	                        $crawler -> addURLFilterRule("#(jpg|jpeg|gif|png|bmp)$# i");
-	                        
-	                        $crawler -> addURLFilterRule("#(css|js)$# i");
-	                        
-	                        $crawler -> enableCookieHandling(true);
-	                        
-	                        $crawler -> setTrafficLimit(100000 * 1024);
-	                        
-	                        $crawler -> go();
-                    	}
-		     ?>
                 </thead>
+                <?php
+					if(!isset($_POST['submit']))
+                    {
+                    }
+                    else
+                    {
+                    $crawler = new MyCrawler();
+                                                
+                    $crawler -> setURL($_POST['url']);
+                                         
+                    $crawler -> addURLFollowRule($_POST['rule']);
+                                          
+                    $crawler -> addContentTypeReceiveRule("#text/html#");
+                                          
+                    $crawler -> addURLFilterRule("#(jpg|jpeg|gif|png|bmp)$# i");
+                                           
+                    $crawler -> addURLFilterRule("#(css|js)$# i");
+                                         
+                    $crawler -> enableCookieHandling(true);
+                                            
+                    $crawler -> setTrafficLimit(100000 * 1024);
+                                            
+                    $crawler -> go();
+                    }
+				?>
             </table>
         </div>
     </div>
 </div>
 <?php
-    include ("footer.php");
+include ("footer.php");
 ?>
